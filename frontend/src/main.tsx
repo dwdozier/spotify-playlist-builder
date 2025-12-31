1b578ac23f73478bd0c7b33166dee44e01514ff7
Dave Dozier <dozier.dave@gmail.com>
2025-12-31 15:18:01 -0500

    fix: import authService in main.tsx to resolve ReferenceError

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { authService } from './api/auth'
 import './index.css'
 
 // Import the generated route tree
